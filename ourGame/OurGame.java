e723fd8079d82a08c7dde689a6e3a0fb14acf266
Nick <nappert@csus.edu>
2022-03-31 16:33:26 -0700

    Put in some skeleton code
    It's not much but it's enough to get a game window running.

--- ourGame/OurGame.java
+++ ourGame/OurGame.java
@@ -1,5 +1,40 @@
 package ourGame;
 
-public class OurGame {
+import tage.*;
+
+public class OurGame extends VariableFrameRateGame {
+    private static Engine engine;
+    public static Engine getEngine() { return engine; }
+    public static void main(String[] args) {
+        OurGame game = new OurGame();
+        engine = new Engine(game);
+        game.initializeSystem();
+        game.game_loop();
+    }
+
+    @Override
+    public void loadShapes() {
+
+    }
+    
+    @Override
+    public void loadTextures() {
+
+    }
+
+    @Override
+    public void buildObjects() {
+
+    }
+
+    @Override
+    public void initializeGame() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
     
 }
